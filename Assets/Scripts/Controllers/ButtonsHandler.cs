541ab8b39907f0543a29b80d9aca4f1e6cf086d7
Leo_Alves <89089486+1LeoAlves@users.noreply.github.com>
2023-11-24 17:22:11 -0300

    Base Commit

--- Assets/Scripts/Controllers/ButtonsHandler.cs
+++ Assets/Scripts/Controllers/ButtonsHandler.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsHandler : MonoBehaviour
 {
-    void Start()
+    [SerializeField] Transform canvasTransformReference;
+    [SerializeField] GameObject faderOutInObject;
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
-
-    void Update()
+    public void ChangeScene(string scenename)
     {
-        
+        SceneManager.LoadScene(scenename);
+    }
+    public void ChangeSceneWithTransition(string scenename)
+    {
+        StartCoroutine(Transition(scenename));
+	}
+    public IEnumerator Transition(string scenename)
+    {
+        Animator faderAnimator = Instantiate(faderOutInObject, canvasTransformReference).GetComponentInChildren<Animator>();
+        yield return new WaitForSeconds(faderAnimator.GetCurrentAnimatorClipInfo(0).Length);
+        ChangeScene(scenename);
     }
 }
