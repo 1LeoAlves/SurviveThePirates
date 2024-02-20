2b03d3a83e629180f7e23c39cc4eb567b45a4841
Leo_Alves <89089486+1LeoAlves@users.noreply.github.com>
2024-02-20 17:46:46 -0300

    SurviveThePiratesEndingPush

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
